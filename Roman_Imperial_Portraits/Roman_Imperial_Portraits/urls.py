163e204de8495f60e65b93ba69b205f0db8e4b89
Thijs_RU <T.Hermsen@let.ru.nl>
2021-05-27 16:26:25 +0200

    After changes to model and creation of superuser

--- Roman_Imperial_Portraits/Roman_Imperial_Portraits/urls.py
+++ Roman_Imperial_Portraits/Roman_Imperial_Portraits/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls import include, url
 import ripdapp.views
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     # Examples:
@@ -15,10 +15,16 @@ urlpatterns = [
     # url(r'^Roman_Imperial_Portraits/', include('Roman_Imperial_Portraits.Roman_Imperial_Portraits.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', ripdapp.views.index, name='index'),
     url(r'^home$', ripdapp.views.index, name='home'),
+    url(r'^about$', ripdapp.views.about, name='about'),
+    url(r'^welcome$', ripdapp.views.welcome, name='welcome'),
+    url(r'^browse$', ripdapp.views.browse, name='browse'),
+    url(r'^advsearch$', ripdapp.views.advsearch, name='advsearch'),
+    url(r'^reflinks$', ripdapp.views.reflinks, name='reflinks'),
+   #url(r'^record$', ripdapp.views.record, name='record')
 ]
--- Roman_Imperial_Portraits/ripdapp/views.py
+++ Roman_Imperial_Portraits/ripdapp/views.py
@@ -5,8 +5,73 @@ from datetime import datetime
 def index(request):
     now = datetime.now()
 
-    html_content = "<html><head><title>Hello, Django</title></head><body>"
-    html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
-    html_content += "</body></html>"
+    return render(
+        request,
+        "ripdapp/index.html",  # Relative path from the 'templates' folder to the template file
+        # "index.html", # Use this code for VS 2017 15.7 and earlier
+        {
+            'title' : "Roman Imperial Portraits Database",
+            'message' : "Roman Imperial Portraits Database",
+            'content' : " on " + now.strftime("%A, %d %B, %Y at %X"),
+            'message2': "This website is made by Thijs Hermsen"
+            #'content': "<strong>Roman Imperial Portraits Database</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
+    #html_content = "<html><head><title>Roman Imperial Portraits Database</title></head><body>"
+    #html_content += "<strong>Roman Imperial Portraits Database</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+    #html_content += "</body></html>"
 
-    return HttpResponse(html_content)
+    #return HttpResponse(html_content)
+
+def about(request):
+    return render(
+        request,
+        "ripdapp/about.html",
+        {
+            'title' : "About Roman Imperial Portraits Database",
+            'content' : "Example app page for Django. This app was developed for the project....",
+            'content2': "Second Example app page for Django. This app was developed for the project...."
+        }
+    )
+
+
+def welcome(request):
+    return render(
+        request,
+        "ripdapp/welcome.html",
+        {
+            'title' : "Welcome Roman Imperial Portraits Database",
+            'content' : "This is the welcome page of the RIPD ",
+            'year':datetime.now().year
+        }
+    )
+
+def browse(request):
+    return render(
+        request,
+        "ripdapp/browse.html",
+        {
+            'title' : "Browse Roman Imperial Portraits Database",
+            'content' : "This is the browse page of the RIPD "            
+        }
+    )
+
+def advsearch(request):
+    return render(
+        request,
+        "ripdapp/advsearch.html",
+        {
+            'title' : "Advanced search Roman Imperial Portraits Database",
+            'content' : "This is the advanced search page of the RIPD "            
+        }
+    )
+
+def reflinks(request):
+    return render(
+        request,
+        "ripdapp/reflinks.html",
+        {
+            'title' : "References and links Roman Imperial Portraits Database",
+            'content' : "This is the references and links page of the RIPD "            
+        }
+    )
